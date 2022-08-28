17e905423d562f42f9a77d44f11cd2222d5166a5
gesora96 <36685490+zahidhasanemran@users.noreply.github.com>
2022-08-29 00:17:57 +0600

    ssp added

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import { Container, Grid } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import ProjectCard from "../components/ProjectCard";
 
-export default function Home() {
+export default function Home({ products }) {
   const styles = useStyles();
+
+  console.log(products);
 
   return (
     <div>
@@ -20,3 +22,12 @@ export default function Home() {
 }
 
 const useStyles = makeStyles({});
+
+export async function getServerSideProps({ req, res }) {
+  // Fetch data from external API
+  const result = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+  const data = await result.json();
+
+  // Pass data to the page via props
+  return { props: { products: data } };
+}
